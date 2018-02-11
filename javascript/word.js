f146d4a4604394ce3447059a8ac48a2afeffadc3
Patrick Knight <ptknight00@gmail.com>
2018-02-11 08:31:53 -0600

    format

--- javascript/word.js
+++ javascript/word.js
@@ -2,20 +2,22 @@ const letter = require('letter.js');
 
 function Word(word) {
     this.characters = [];
-    for (var letter of word) {//add each letter to array characters
+    for (var letter of word) { //add each letter to array characters
         this.characters.push(new Letter(letter));
     }
-    this.visible = function (){//word displayed
-        let charArr = this.characters.map(function(letter) {
+    this.visible = function () { //word displayed
+        let charArr = this.characters.map(function (letter) {
             letter.show().join(' ');
         });
         return charArr;
     }
-    this.check = function(char){
-        this.characters.forEach(x => {x.compare(char)})
+    this.check = function (char) {
+        this.characters.forEach(x => {
+            x.compare(char)
+        })
         this.visible();
     }
-};//close Word
+}; //close Word
 
 
 module.exports = Word;
